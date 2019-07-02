b65483c2d793872cb0570c4c0f94116c3421e7ab
Ron Ilan <git@ronilan.com>
2019-07-02 13:12:19 -0700

    better dev setup. npm run start to watch files with hot reload. npm run build for lint and minified version.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,11 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  devServer: {
+    contentBase: __dirname,
+    watchContentBase: true,
+    writeToDisk: true
+  },
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'js')
