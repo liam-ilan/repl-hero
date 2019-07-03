e4827921b432919af8644bc03d26e25411e74763
Ron Ilan <git@ronilan.com>
2019-07-02 19:45:57 -0700

    refactor player and index in prep for visualization. loop is now part of index. player's sole responsability is to play the chord.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import player from './player';
 
-player();
+import generate from './generator';
+import playChord from './player'
+
+function loop(beatLength = 500) {
+  // initilize to random bar
+  let currentBar = generate();
+  let beatNum = 0;   // between 0 to 3
+  let pitch = Math.floor((Math.random() * 3) + 3);   // random pitch
+
+  // loop on every beat
+  window.setInterval(() => {
+    // play chord
+    playChord(currentBar.content[beatNum], pitch);
+
+    // generate new bar once bar
+    if (beatNum === 3) {
+      currentBar = generate(currentBar);
+      pitch = Math.floor((Math.random() * 3) + 3);
+      beatNum = 0;
+    } else {
+      beatNum += 1;
+    }
+  }, beatLength);
+}
+
+loop();
